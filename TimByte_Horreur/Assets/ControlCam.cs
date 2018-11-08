c9f6745b84b4ec1577016aecf43a3f4ac0da64a9
Phil <philippe.thibeault99@outlook.com>
2018-11-08 00:48:42 -0500

    Deplacement Perso

---
+++ TimByte_Horreur/Assets/ControlCam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCam : MonoBehaviour {
+
+    public float minX = -60f;
+    public float maxX = 60f;
+    public float minY = -360f;
+    public float maxY = 360f;
+
+    public float sensibiliterX = 15f;
+    public float sensibiliterY = 15f;
+    public Vector3 offset;
+
+    public Camera cam;
+
+    float rotY = 0f;
+    float rotX = 0f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        rotY += Input.GetAxis("Mouse X") * sensibiliterY;
+        rotX += Input.GetAxis("Mouse Y") * sensibiliterX;
+
+        rotX = Mathf.Clamp(rotX, minX, maxX);
+
+        transform.localEulerAngles = new Vector3(0, rotY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotX, rotY, 0);
+
+        cam.transform.position = transform.position + offset;
+    }
+}
---
+++ TimByte_Horreur/Assets/DeplacementPerso.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementPerso : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    public float vitesseDep;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        /*var depVert = Input.GetAxis("Vertical");
+        var depHor = Input.GetAxis("Horizontal");
+
+        float vitHor = depHor * vitesseDep;
+        float vitVert = depVert * vitesseDep;
+
+        Vector3 velY = new Vector3(0, rb.velocity.y, 0);
+        rb.velocity = new Vector3(vitHor, 0, vitVert).normalized;
+        rb.velocity += velY;*/
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * vitesseDep;
+        }
+
+        /*Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit infoCollisionPerso;
+
+        if (Physics.Raycast(camRay.origin, camRay.direction, out infoCollisionPerso, 5000))
+        {
+            Vector3 pointARegarder = infoCollisionPerso.point;//(x, y, z)
+            /*gameObject.*/
+            /*transform.LookAt(pointARegarder);
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+        }*/
+    }
+}
